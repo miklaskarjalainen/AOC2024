c2655dfdc1aebe12e0266b1f33ecc5cd624894b4
Giffi <miklas.karjalainen@gmail.com>
2024-12-02 15:49:52 +0200

    AOC DAY02

--- day-02/Program.cs
+++ day-02/Program.cs
@@ -1,58 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] reports = File.ReadAllLines("input.txt");
 
-string[] reports = File.ReadAllLines("input2.txt");
+Int64 part1_count = 0;
+Int64 part2_count = 0;
 
-Int64 safe_count = 0;
+bool part1_valid(List<Int64> levels) {
+    bool decreasing = true;
 
-foreach (string report in reports) {
-    bool safe = true;
-    Int64? last_num = null; 
-    bool? decreasing = null;
-
-
-    string[] levels = report.Split(' ');
-
-    foreach (string level in levels) {
-        Console.Write(level + " ");
-
-
-        Int64 num = Int64.Parse(level);
-        if (last_num == null) {
-            last_num = num;
-            continue;
+    for (int i = 1; i < levels.Count; i++) {
+        Int64 diff = levels[i-1] - levels[i];
+        if (Math.Abs(diff) < 1 || Math.Abs(diff) > 3) {
+            return false;
         }
 
-        Int64 difference = num - (Int64)last_num;
-        last_num = num;
-        if (Math.Abs(difference) < 1 || Math.Abs(difference) > 3) {
-            safe = false;
-            break;
-        }
+        bool is_decreasing = diff < 0;
 
-        bool is_decreasing = difference < 0;
-        if (decreasing == null) {
+        if (i == 1) {
             decreasing = is_decreasing;
             continue;
         }
-        else if (is_decreasing != decreasing) {
-            safe = false;
-            break;
+        
+        if (decreasing != is_decreasing) {
+            return false;
         }
-
     }
 
-
-
-    if (safe) {
-        Console.WriteLine("Safe!");
-        safe_count++;
-    }
-    else {
-        Console.WriteLine("UnSafe!");
-    }
-
+    return true;
 }
-Console.WriteLine(safe_count);
+
+bool part2_valid(List<Int64> levels) {
+    for (int i = 0; i < levels.Count; i++) {
+        List<Int64> dup = [.. levels];
+        dup.RemoveAt(i);
+
+        if (part1_valid(dup)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+foreach (string report in reports) {
+    string[] levels = report.Split(' ');
+
+    List<Int64> int_levels = new();
+    foreach (string level in levels) {
+        int_levels.Add(Int64.Parse(level));
+    }
+
+    if (part1_valid(int_levels)) {
+        part1_count++;
+        part2_count++;
+        continue;
+    }
+    if (part2_valid(int_levels)) {
+        part2_count++;
+        continue;
+    }
+}
+
+Console.WriteLine($"Part1 results: {part1_count}");
+Console.WriteLine($"Part2 results: {part2_count}");
 
 
